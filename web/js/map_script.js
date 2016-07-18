40e2fb70d01911489ccbc85e62f93f7194af1e08
noamloya <noamloya@gmail.com>
2016-07-18 19:40:56 +0300

    adding

---
+++ web/js/map_script.js
@@ -0,0 +1,48 @@
+function initMap() {
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 44.540, lng: -78.546},
+            zoom: 8
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+/*** ADDING LOCATIONS TO MAP ***/
+        var contentString = ''
+        $.getJSON("../resources/locations.json", function(json) {
+              json.locations.forEach(function(element){
+                var latLng = {lat: element.lat, lng: element.lng};
+                contentString = '<div id="content">'+
+                    '<div id="content_'+element.id+'">'+
+                    '</div>'+
+                    '<h3 id="firstHeading'+element.id+'" class="firstHeading">'+element.title+'</h3>'+
+                    '<div id="bodyContent'+element.id+'">'+ element.info +
+                    '</div>'+
+                    '</div>';
+                var infoWindow = new google.maps.InfoWindow({
+                  content: contentString
+                });
+                var marker = new google.maps.Marker({
+                  position: latLng,
+                  map: map,
+                  title: element.title
+                });
+                marker.addListener('click', function() {
+                  infoWindow.open(map, marker);
+                });
+            })
+        });
+}
