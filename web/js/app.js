d05382d5259ebf3e055c1ccf2d842700ff90be9f
Jonathan A <jony543@gmail.com>
2016-07-19 08:53:57 +0300

    added lazy script directive; discover now works in view

--- web/js/app.js
+++ web/js/app.js
@@ -20,6 +20,20 @@ inkApp.config(['$routeProvider',
         });
     }]);
 
+inkApp.directive('script', function() {
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function(scope, elem, attr) {
+            if (attr.type === 'text/javascript-lazy') {
+                var code = elem.text();
+                var f = new Function(code);
+                f();
+            }
+        }
+    };
+});
+
 inkApp.controller('HomeController', function($scope) {
 
     $scope.message = 'Home Screen';
